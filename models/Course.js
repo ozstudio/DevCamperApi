466e96d83f569d4dfbc9c406ad4bb9a9df3c2fd3
OL <ozstudiominigames@gmail.com>
2023-04-02 21:39:53 +0300

    Average Bootcamp cost added

--- models/Course.js
+++ models/Course.js
@@ -37,4 +37,49 @@ const CourseSchema = new mongoose.Schema({
     required: true
   }
 });
+
+//static method get avg xourse tuition
+CourseSchema.statics.getAverageCost = async function(bootcampId){
+    console.log('calculating avg cost ...'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match:{bootcamp :bootcampId}
+        },
+        {
+            $group:{
+                _id:'$bootcamp',
+                averageCost:{$avg:'$tuition'}
+
+            }
+        }
+    ])
+    try {
+            //obj returns (1) array,after we extracting 'obj[0].averageCost'
+            //
+        //(1) [
+        //     { _id: new ObjectId("5d725a1b7b292f5f8ceff788"), averageCost: 9000 }
+        //   ]
+        //obj returns array
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId,{
+            averageCost:Math.ceil(obj[0].averageCost/10)*10
+        })
+        
+    } catch (error) {
+        console.error(err);
+    }
+
+}
+
+//call getAverageCost after save
+CourseSchema.post('save',function(){
+    this.constructor.getAverageCost(this.bootcamp);
+
+});
+
+//call getAverageCost before save
+CourseSchema.pre('save',function(){
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
